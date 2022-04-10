1e9b6024f7cb09431dabeef95ccdab3a4e205359
zhaohu <zhaohu@123.com>
2022-04-11 07:10:53 +0800

    双指针 逆序问题

--- src/com/zhaohu/basic/array/RemoveRepetition.java
+++ src/com/zhaohu/basic/array/RemoveRepetition.java
@@ -1,39 +1,77 @@
 package com.zhaohu.basic.array;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 /**
  * 移除按照升序排序的数组中重复的元素
  * 要求，原地
  */
 public class RemoveRepetition {
     public static void main(String[] args) {
-        int[] nums=new int[]{1,1,2};
-        int s=removeDuplicates1(nums);
+        int[] nums = new int[]{1, 1, 2};
+        int s = removeDuplicates1(nums);
     }
 
     //双指针
-    private static int removeDuplicates(int[] nums){
+    private static int removeDuplicates(int[] nums) {
 
-        int result=0;
+        int result = 0;
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i]!=nums[result]){
+            if (nums[i] != nums[result]) {
                 result++;
-                nums[result]=nums[i];
+                nums[result] = nums[i];
             }
         }
 
-        return result +1;
+        return result + 1;
     }
 
-    private static int removeDuplicates1(int[] nums){
-        int count=0;
+    private static int removeDuplicates1(int[] nums) {
+        int count = 0;
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i]==nums[i-1]){
+            if (nums[i] == nums[i - 1]) {
                 count++;
-            }else {
-                nums[i-count]=nums[i];
+            } else {
+                nums[i - count] = nums[i];
             }
         }
 
-        return nums.length-count;
+        return nums.length - count;
     }
+
+    //双指针其他问题
+    //输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+    //递归解决
+    private static int[] reversePrint(ListNode head) {
+        ArrayList<Integer> result=new ArrayList<>();
+        if(head!=null)
+            getValue(head,result);
+
+        int[] res=new int[result.size()];
+
+        int i=0;
+        for (Integer r:result) {
+            res[i++]=r;
+        }
+
+        return res;
+    }
+
+    private static void getValue(ListNode node,ArrayList<Integer> result){
+        if(node.next!=null){
+            getValue(node.next,result);
+        }
+        result.add(node.val);
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
 }
---
+++ src/com/zhaohu/basic/array/TheBestTime.java
@@ -0,0 +1,23 @@
+package com.zhaohu.basic.array;
+
+/**
+ * 给定一个数组 prices ，其中 prices[i] 表示股票第 i 天的价格。
+ *
+ * 在每一天，你可能会决定购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以购买它，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2zsx1/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class TheBestTime {
+    public static void main(String[] args) {
+
+    }
+
+    private static int maxProfit(int[] prices){
+
+        return 1;
+    }
+}
