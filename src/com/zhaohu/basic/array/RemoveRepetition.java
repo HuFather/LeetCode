38abde3fc56dc347799a3ef5404b7cbd18c29d7d
zhaohu <zhaohu@gridsum.com>
2022-04-11 20:01:21 +0800

    三数和为0

--- src/com/zhaohu/basic/array/RemoveRepetition.java
+++ src/com/zhaohu/basic/array/RemoveRepetition.java
@@ -1,6 +1,6 @@
 package com.zhaohu.basic.array;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.stream.IntStream;
 
 /**
@@ -10,7 +10,8 @@ import java.util.stream.IntStream;
 public class RemoveRepetition {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 1, 2};
-        int s = removeDuplicates1(nums);
+//        int s = removeDuplicates1(nums);
+        List<List<Integer>> result = threeSum(new int[]{-2,0,1,1,2});
     }
 
     //双指针
@@ -41,26 +42,66 @@ public class RemoveRepetition {
     }
 
     //双指针其他问题
+    //给定一个包含 n 个整数的数组nums，判断nums中是否存在三个元素a ，b ，c ，使得a + b + c = 0 ？请找出所有和为 0 且不重复的三元组。
+    //
+    //来源：力扣（LeetCode）
+    //链接：https://leetcode-cn.com/problems/1fGaJU
+    //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    private static List<List<Integer>> threeSum(int[] nums) {
+        nums = Arrays.stream(nums).sorted().toArray();
+        List<List<Integer>> result = new ArrayList<>();
+        Set<String> duplicates = new HashSet<>();
+        int index = 1, end = nums.length - 1;
+        for (int start = 0; start < end - 1 && index < end; start++) {
+            int temp = nums[start] + nums[index] + nums[end];
+            if (temp == 0) {
+                List<Integer> res = new ArrayList<>();
+
+                res.add(nums[start]);
+                res.add(nums[index]);
+                res.add(nums[end]);
+                String str = nums[start] + "" + nums[index] + "" + nums[end];
+                if (!duplicates.contains(str)) {
+                    duplicates.add(str);
+                    result.add(res);
+                }
+                index++;
+            } else if (temp > 0) {
+                end--;
+                start--;
+            } else {
+                if (index == end - 1) {
+                    index = start + 2;
+                } else {
+                    index++;
+                    start--;
+                }
+            }
+        }
+
+        return result;
+    }
+
     //输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
     //递归解决
     private static int[] reversePrint(ListNode head) {
-        ArrayList<Integer> result=new ArrayList<>();
-        if(head!=null)
-            getValue(head,result);
+        ArrayList<Integer> result = new ArrayList<>();
+        if (head != null)
+            getValue(head, result);
 
-        int[] res=new int[result.size()];
+        int[] res = new int[result.size()];
 
-        int i=0;
-        for (Integer r:result) {
-            res[i++]=r;
+        int i = 0;
+        for (Integer r : result) {
+            res[i++] = r;
         }
 
         return res;
     }
 
-    private static void getValue(ListNode node,ArrayList<Integer> result){
-        if(node.next!=null){
-            getValue(node.next,result);
+    private static void getValue(ListNode node, ArrayList<Integer> result) {
+        if (node.next != null) {
+            getValue(node.next, result);
         }
         result.add(node.val);
     }
