f28f36dc9cf24f8876511a7ccded6b67339be6da
zhaohu <zhaohu@123.com>
2022-04-13 22:34:14 +0800

    按层遍历二叉树

---
+++ src/com/zhaohu/temporary/BinaryTree.java
@@ -0,0 +1,47 @@
+package com.zhaohu.temporary;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTree {
+
+    public List<Integer> rightNum(ListNode node){
+        List<Integer> result=new ArrayList<>();
+        if(node==null){
+            return result;
+        }
+
+        LinkedList<ListNode> nodes=new LinkedList<>();
+        nodes.add(node);
+        while (!nodes.isEmpty()){
+            int length=nodes.size();
+            for (int i = 0; i < length; i++) {
+
+                ListNode currentNode=nodes.poll();
+                if(i==length-1){
+                    result.add(currentNode.val);
+                }
+                if(currentNode.left!=null){
+                    nodes.add(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    nodes.add(currentNode.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+class ListNode {
+    int val;
+    public ListNode left;
+    public ListNode right;
+    ListNode(int x) {
+        val = x;
+    }
+}
