17753f7c7dd5b7f62747659813778279ec544fc7
zhaohu <zhaohu@123.com>
2022-04-24 21:05:55 +0800

    数组交集、移动0、加1、重复数字

---
+++ src/com/zhaohu/basic/array/IntersectionCollection.java
@@ -0,0 +1,32 @@
+package com.zhaohu.basic.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IntersectionCollection {
+    public static int[] intersection(int[] nums1,int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> result=new ArrayList<>();
+        int l1=0;
+        int l2=0;
+        while (l1<nums1.length && l2<nums2.length){
+            if(nums1[l1]==nums2[l2]){
+                result.add(nums1[l1]);
+                l1++;
+                l2++;
+            }else if(nums1[l1]<nums2[l2]){
+                l1++;
+            }
+            else {
+                l2++;
+            }
+        }
+        int[] last=new int[result.size()];
+        for (int i = 0; i < last.length; i++) {
+            last[i]=result.get(i);
+        }
+        return last;
+    }
+}
