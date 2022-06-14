4f1d6c11e7419cbe344a98600d7110285ad86fe8
zhaohu <zhaohu@gridsum.com>
2022-06-14 11:37:46 +0800

    up

--- src/com/zhaohu/medium/sort/Solution.java
+++ src/com/zhaohu/medium/sort/Solution.java
@@ -1,11 +1,15 @@
 package com.zhaohu.medium.sort;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+
 public class Solution {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 0, 2, 1, 1, 0};
+        int[] nums = new int[]{3,3,5,5,3,5,5,6};
 
-        sortColors(nums);
+        int[] result= topKFrequent(nums,2);
     }
 
     /**
@@ -36,4 +40,43 @@ public class Solution {
         nums[left] = nums[right];
         nums[right] = temp;
     }
+
+
+    /**
+     * 找出出现频率前k高的元素
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] result=new int[k];
+        int[] maxs=new int[k];
+        for (int i = 0; i < nums.length; i++) {
+            Integer value= map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+        }
+
+        return result;
+
+    }
+
+    public int[] topKFrequent1(int[] nums, int k) {
+        //先统计每个数字出现的次数
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        //最大堆
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (int key : map.keySet())
+            priorityQueue.add(new int[]{key, map.get(key)});
+
+        //取堆中最大的k个元素
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++)
+            res[i] = priorityQueue.poll()[0];
+        return res;
+    }
+
 }
