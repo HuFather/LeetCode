f941eec1544cc886ef8f7b317b40be4370f0e401
zhaohu <zhaohu@gridsum.com>
2022-06-15 18:20:49 +0800

    up

--- src/com/zhaohu/medium/sort/Solution.java
+++ src/com/zhaohu/medium/sort/Solution.java
@@ -7,9 +7,10 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3,3,5,5,3,5,5,6};
+        int[] nums = new int[]{4,3, 3, 5, 5, 3, 5, 5, 6};
 
-        int[] result= topKFrequent(nums,2);
+//        int[] result= topKFrequent(nums,2);
+        findKthLargest1(nums, 3);
     }
 
     /**
@@ -44,16 +45,17 @@ public class Solution {
 
     /**
      * 找出出现频率前k高的元素
+     *
      * @param nums
      * @param k
      * @return
      */
     public static int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[] result=new int[k];
-        int[] maxs=new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[k];
+        int[] maxs = new int[k];
         for (int i = 0; i < nums.length; i++) {
-            Integer value= map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            Integer value = map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
         }
 
@@ -77,6 +79,75 @@ public class Solution {
         for (int i = 0; i < k; i++)
             res[i] = priorityQueue.poll()[0];
         return res;
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (int i = 0; i < nums.length; i++) {
+            queue.add(nums[i]);
+        }
+
+        for (int i = 0; i < k; i++) {
+            int result = queue.poll();
+            if (i == k - 1)
+                return result;
+        }
+
+        return -1;
+    }
+
+    /**
+     * 基于快排
+     * k = nums.length - k;//注意这里的k已经变了
+     * int lo = 0, hi = nums.length - 1;
+     * while (lo <= hi) {
+     * int i = lo;
+     * //这里把数组以A[lo]的大小分为两部分，
+     * //一部分是小于A[lo]的，一部分是大于A[lo]的
+     * // [lo,i]<A[lo]，[i+1,j)>=A[lo]
+     * for (int j = lo + 1; j <= hi; j++)
+     * if (nums[j] < nums[lo])
+     * swap(nums, j, ++i);
+     * swap(nums, lo, i);
+     * if (k == i)
+     * return nums[i];
+     * else if (k < i)
+     * hi = i - 1;
+     * else
+     * lo = i + 1;
+     * }
+     * return -1;
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int findKthLargest1(int[] nums, int k) {
+        k = nums.length - k;//注意这里的k已经变了
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int i = lo;
+            //这里把数组以A[lo]的大小分为两部分，
+            //一部分是小于A[lo]的，一部分是大于A[lo]的
+            // [lo,i]<A[lo]，[i+1,j)>=A[lo]
+            for (int j = lo + 1; j <= hi; j++)
+                if (nums[j] < nums[lo])
+                    swap(nums, j, ++i);
+            swap(nums, lo, i);
+            if (k == i)
+                return nums[i];
+            else if (k < i)
+                hi = i - 1;
+            else
+                lo = i + 1;
+        }
+        return -1;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 
 }
