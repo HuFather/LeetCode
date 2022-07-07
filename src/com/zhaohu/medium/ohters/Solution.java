b09c4586850bbb5ebac4d7285fb07a21fb319baa
zhaohu <zhaohu@gridsum.com>
2022-07-07 16:31:45 +0800

    up

--- src/com/zhaohu/medium/ohters/Solution.java
+++ src/com/zhaohu/medium/ohters/Solution.java
@@ -102,4 +102,42 @@ public class Solution {
         public abstract int execute(int left, int right);
     }
 
+    /**
+     * 【1，2，3，3，3，2】
+     * 找出多数数字
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int count=1;
+        int num=nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if(count==0){
+                count=1;
+                num=nums[i];
+            }else {
+                if(num==nums[i])
+                    count++;
+                else {
+                    count--;
+                }
+            }
+        }
+        return num;
+    }
+
+    /**
+     * 给你一个用字符数组tasks 表示的 CPU 需要执行的任务列表。其中每个字母表示一种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。在任何一个单位时间，CPU 可以完成一个任务，或者处于待命状态。
+     * 然而，两个 相同种类 的任务之间必须有长度为整数 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+     * 你需要计算完成所有任务所需要的 最短时间
+     * @param tasks
+     * @param n
+     * @return
+     */
+    public int leastInterval(char[] tasks, int n) {
+
+        return 1;
+    }
+
 }
